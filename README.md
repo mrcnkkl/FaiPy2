30be054295168f16ddf27d928d378ea216b19e03
mrcn.kkl <mrcn.kkl@gmail.com>
2019-04-28 17:07:23 +0200

    hrk 1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-FaiPy
-=====
+
 [![Build Status](https://travis-ci.org/mrcnkkl/FaiPy2.svg?branch=master)](https://travis-ci.org/mrcnkkl/FaiPy2) [![Coverage Status](https://coveralls.io/repos/github/mrcnkkl/FaiPy2/badge.svg?branch=master)](https://coveralls.io/github/mrcnkkl/FaiPy2?branch=master&service=github)
+===
+# FaiPy
 
 FakturowniaApaczkaIntegrator
--- fai/__init__.py
+++ fai/__init__.py
@@ -6,7 +6,16 @@ def create_app(config=None):
     app = Flask(__name__)
 
     app.config.from_mapping(
-        SECRET_KEY=os.getenv('FLASK_SECRET_KEY', default=None)
+        SECRET_KEY=os.getenv('FLASK_SECRET_KEY', default=None),
+        FLASK_ENV = os.getenv('FLASK_ENV', default='production')
     )
 
+    @app.route('/')
+    def test():
+        return 'mrcn hello test 1'
+
     return app
+
+
+app = create_app()
+app.run()
