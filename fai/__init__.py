3158c2d48a6cbd1305db1d641e27e6f84ccc6a6b
mrcn.kkl <mrcn.kkl@gmail.com>
2019-04-30 19:28:50 +0200

    login1+requirements

--- fai/__init__.py
+++ fai/__init__.py
@@ -1,14 +1,21 @@
 import os
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 
 def create_app(config=None):
+
     app = Flask(__name__)
+    login_manager.init_app(app)
 
     app.config.from_mapping(
         SECRET_KEY=os.getenv('FLASK_SECRET_KEY', default=None),
         # SECRET_KEY='secret',
-        FLASK_ENV = os.getenv('FLASK_ENV', default='production')
+        FLASK_ENV=os.getenv('FLASK_ENV', default='production')
     )
 
     @app.route('/test')
--- fai/auth/routing.py
+++ fai/auth/routing.py
@@ -1,12 +1,17 @@
 from flask import (current_app, Blueprint, render_template, flash, redirect, url_for)
 from fai.auth.forms import LoginForm
+from fai import bcrypt
+import os
 
 authbp = Blueprint('login', __name__)
+
 
 @authbp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Jesteś zalogowany', 'success')
+        if form.username.data == os.getenv('FAI_USER') \
+                and bcrypt.check_password_hash('$2b$12$OSa5/UxaG/lujCKhWvYUSuq.O/ggl4G9icoGAbgtvPAet/ow0n2Sy', form.password.data):
+            flash('Jesteś zalogowany', 'success')
         return redirect(url_for('home.home'))
     return render_template('login.html', form=form)
