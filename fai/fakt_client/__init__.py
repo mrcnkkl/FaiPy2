5fd501d53046a5e51e629ba4ea057998157c85dd
mrcn.kkl <mrcn.kkl@gmail.com>
2019-05-07 19:16:23 +0200

    faktur-apac1

--- fai/fakt_client/__init__.py
+++ fai/fakt_client/__init__.py
@@ -12,3 +12,10 @@ def getinvoicebyid(id: str):
     URL = f'https://{config.fakt_domain}.fakturownia.pl/invoices/{id}.json?api_token={config.fakt_token}'
     response = requests.get(URL)
     return response.json()
+
+
+def getclientbyid(id: str):
+    '''curl "https://YOUR_DOMAIN.fakturownia.pl/clients/100.json?api_token=API_TOKEN"'''
+    URL = f'https://{config.fakt_domain}.fakturownia.pl/clients/{id}.json?api_token={config.fakt_token}'
+    response = requests.get(URL)
+    return  response.json()
